b53721b3e43c99287f600d41464c755f06d42bf8
Andrea Bruno <andybbruno@gmail.com>
2020-04-08 19:14:13 +0200

    Sequential finished

--- Assignment/gol_seq.cpp
+++ Assignment/gol_seq.cpp
@@ -1,46 +1,65 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include "utils.hpp"
 
-void print(std::vector<std::vector<bool>> const &vec)
-{
-    std::cout << std::string(vec[0].size(), '*') + "\t" << std::endl
-              << std::endl;
-
-    for (std::vector<bool> x : vec)
-    {
-        for (bool b : x)
-        {
-            std::cout << (b == true ? '*' : ' ') << "\t";
-        }
-        std::cout << std::endl;
-    }
-
-    std::cout << std::string(vec[0].size(), '*') << std::endl
-              << std::endl;
-}
 
 int main(int argc, char *argv[])
 {
     size_t niter = atoi(argv[1]);
+
     size_t seed = atoi(argv[2]);
+    (seed == 0) ? srand(time(NULL)) : srand(seed);
+
     size_t n = atoi(argv[3]);
     size_t m = atoi(argv[4]);
     size_t nw = atoi(argv[5]);
 
-    srand(seed);
-
     std::vector<std::vector<bool>> matrix(n, std::vector<bool>(m, false));
 
-    size_t offset = 1;
-    for (size_t i = 1; i < n - 1; i += offset)
+    matrix::randomize(matrix);
+
+    // create_glider(matrix, 1, 1);
+    // create_glider(matrix, 1, 10);
+
+    matrix::print(matrix);
+
+    for (size_t k = 0; k < niter; k++)
     {
-        for (size_t j = 1; j < m - 1; j += offset)
+        std::vector<std::vector<bool>> matrix_new(n, std::vector<bool>(m, false));
+
+        for (size_t i = 1; i < n - 1; i++)
         {
-            matrix[i][j] = (rand() % 2) == 0 ? false : true;
+            for (size_t j = 1; j < m - 1; j++)
+            {
+                int neig = game::neighbourhood(matrix, i, j);
+                bool alive = matrix[i][j];
+
+                if (alive)
+                {
+                    if ((neig < 2) | (neig > 3))
+                    {
+                        matrix_new[i][j] = false;
+                    }
+                    else if ((neig == 2) | (neig == 3))
+                    {
+                        matrix_new[i][j] = true;
+                    }
+                }
+                else
+                {
+                    if (neig == 3)
+                    {
+                        matrix_new[i][j] = true;
+                    }
+                }
+            }
         }
+
+        matrix = matrix_new;
+        matrix::print(matrix);
+        utils::delay(100);
     }
 
-    print(matrix);
     return 0;
 }
---
+++ Assignment/utils.hpp
@@ -0,0 +1,94 @@
+#pragma once
+
+namespace utils
+{
+void delay(int msecs)
+{
+    // read current time
+    auto start = std::chrono::high_resolution_clock::now();
+    auto end = false;
+    while (!end)
+    {
+        auto elapsed = std::chrono::high_resolution_clock::now() - start;
+        auto msec = std::chrono::duration_cast<std::chrono::milliseconds>(elapsed).count();
+        if (msec > msecs)
+            end = true;
+    }
+    return;
+}
+} // namespace utils
+
+namespace game
+{
+int neighbourhood(std::vector<std::vector<bool>> const &vec, uint ix, uint jx)
+{
+    uint counter = 0;
+    bool alive = vec[ix][jx];
+
+    for (size_t i = ix - 1; i < ix + 2; i++)
+    {
+        for (size_t j = jx - 1; j < jx + 2; j++)
+        {
+            if (vec[i][j] == true)
+                counter++;
+        }
+    }
+    return alive ? --counter : counter;
+}
+
+void create_glider(std::vector<std::vector<bool>> &vec, uint i, uint j)
+{
+    // Glider 1
+    vec[1 + i][3 + j] = true;
+    vec[2 + i][1 + j] = true;
+    vec[2 + i][3 + j] = true;
+    vec[3 + i][2 + j] = true;
+    vec[3 + i][3 + j] = true;
+}
+} // namespace game
+
+namespace matrix
+{
+void randomize(std::vector<std::vector<bool>> &vec)
+{
+    uint n = vec.size();
+    uint m = vec[0].size();
+
+    for (size_t i = 1; i < n - 1; i++)
+    {
+        for (size_t j = 1; j < m - 1; j++)
+        {
+            vec[i][j] = (rand() % 2) == 0 ? false : true;
+        }
+    }
+}
+void print(std::vector<std::vector<bool>> const &vec)
+{
+    size_t rows = vec.size();
+    size_t cols = vec[0].size();
+
+    std::cout << std::endl;
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            bool b = vec[i][j];
+            if (j == 0)
+            {
+                std::cout << (b == true ? "●" : " ") << "|";
+            }
+            else if (j == cols - 1)
+            {
+                std::cout << (b == true ? "●" : " ");
+            }
+            else
+            {
+                std::cout << (b == true ? "●" : " ") << "|";
+            }
+        }
+        std::cout << std::endl;
+    }
+    std::cout << std::endl;
+}
+} // namespace matrix
