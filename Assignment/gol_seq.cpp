395719d996939f2b12f5c968f6afd0353bd2e388
Andrea Bruno <andybbruno@gmail.com>
2020-04-08 15:22:15 +0200

    Update gol_seq.cpp

--- Assignment/gol_seq.cpp
+++ Assignment/gol_seq.cpp
@@ -1,12 +1,27 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
 int main(int argc, char *argv[])
 {
-    uint64_t niter = atoi(argv[1]);
-    uint64_t seed = atoi(argv[2]);
-    uint64_t n = atoi(argv[3]);
-    uint64_t m = atoi(argv[4]);
-    uint64_t nw = atoi(argv[5]);
-    
-    std::cout << "CIAO" << std::endl;
+    size_t niter = atoi(argv[1]);
+    size_t seed = atoi(argv[2]);
+    size_t n = atoi(argv[3]);
+    size_t m = atoi(argv[4]);
+    size_t nw = atoi(argv[5]);
+
+    srand(seed);
+
+    std::vector<std::vector<bool>> matrix;
+
+    for (size_t i = 0; i < n; i++)
+    {
+        std::vector<bool> tmp(m);
+        std::for_each(tmp.begin(),
+                      tmp.end(),
+                      [&](bool x) { return (rand() % 2) == 0 ? false : true; });
+        matrix.push_back(tmp);
+    }
+
+    return 0;
 }
