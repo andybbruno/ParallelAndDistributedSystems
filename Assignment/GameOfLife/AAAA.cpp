659e8842a7c4ca8c6c60b66f6e97d3bf3b2ffc5d
Andrea Bruno <andybbruno@gmail.com>
2020-04-11 18:30:38 +0200

    fix

--- Assignment/GameOfLife/AAAA.cpp
+++ Assignment/GameOfLife/AAAA.cpp
@@ -72,25 +72,25 @@ int main(int argc, char *argv[])
 
     utimer u("Parallel");
 
-    auto emit_thr = new std::thread(emit, emitter);
-    
-    std::vector<std::thread *> tids(nw);
+    // auto emit_thr = new std::thread(emit, emitter);
+    std::thread emit_thr(emit, emitter);
+
+    std::vector<std::thread> tids;
     for (int i = 0; i < nw; i++)
     {
-        tids[i] = new std::thread(work, worker, i);
+        tids.push_back(std::thread(work, worker, i));
     }
 
-    auto collect_thr = new std::thread(collect, collector);
+    std::thread collect_thr(collect, collector);
 
-
-    emit_thr->join();
+    emit_thr.join();
 
     for (int i = 0; i < nw; i++)
-	{
-        tids[i]->join();
+    {
+        tids[i].join();
     }
 
-    collect_thr->join();
+    collect_thr.join();
 
     return 0;
 }
