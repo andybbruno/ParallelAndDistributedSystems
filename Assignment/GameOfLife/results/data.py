fd54142a42318fc1541b8c19bc8ae02b8fe11e70
Andrea Bruno <andybbruno@gmail.com>
2020-04-17 19:23:00 +0200

    Results

--- Assignment/GameOfLife/results/data.py
+++ Assignment/GameOfLife/results/data.py
@@ -5,6 +5,7 @@ df0 = pd.read_csv("seq.csv")
 df1 = pd.read_csv("par_active.csv")
 df2 = pd.read_csv("par_passive.csv")
 df3 = pd.read_csv("par_omp.csv")
+df4 = pd.read_csv("par_ff.csv")
 
 
 # for nw in [4, 8, 16, 32, 64, 128, 256]:
@@ -15,6 +16,7 @@ seq = df0.groupby(['nw', 'rowcol']).mean().reset_index()
 par1 = df1.groupby(['nw', 'rowcol']).mean().reset_index()
 par2 = df2.groupby(['nw', 'rowcol']).mean().reset_index()
 par3 = df3.groupby(['nw', 'rowcol']).mean().reset_index()
+par4 = df4.groupby(['nw', 'rowcol']).mean().reset_index()
 
 
 for nw in [1, 4, 16, 64, 256]:
@@ -22,16 +24,18 @@ for nw in [1, 4, 16, 64, 256]:
     tmp1 = par1[(par1.nw == nw)]
     tmp2 = par2[(par2.nw == nw)]
     tmp3 = par3[(par3.nw == nw)]
-    a, = plt.plot(seq['rowcol'], seq['time'], 'o-b', label='Seq')
+    tmp4 = par4[(par4.nw == nw)]
+    a, = plt.plot(seq['rowcol'], seq['time'], 'o-k', label='Seq')
     b, = plt.plot(tmp1['rowcol'], tmp1['time'], '-*r', label='ActiveFarm')
     c, = plt.plot(tmp2['rowcol'], tmp2['time'], 's-.g', label='PassiveFarm')
     d, = plt.plot(tmp3['rowcol'], tmp3['time'], '^--y', label='ParallelForOMP')
+    e, = plt.plot(tmp4['rowcol'], tmp4['time'], ':Dm', label='ParallelForFF')
     plt.yscale('log')
     plt.xscale('log')
     plt.ylabel('Time')
     plt.xlabel('Size')
     plt.title("NW: " + str(nw))
-    plt.legend(handles=[a,b,c,d])
+    plt.legend(handles=[a,b,c,d,e])
     plt.show()
     fig.savefig('time_size' + str(nw) + '.png')
 
@@ -42,17 +46,21 @@ for col in [256, 1024, 4096, 16384]:
     tmp1 = par1[(par1.rowcol == col)]
     tmp2 = par2[(par2.rowcol == col)]
     tmp3 = par3[(par3.rowcol == col)]
-    # plt.plot(seq['rowcol'], seq['time'], 'o-b')
-    plt.plot([0, 256], [0, 256])
+    tmp4 = par4[(par4.rowcol == col)]
+
+
+    plt.plot([0, 256], [0, 256],'k')
     a, = plt.plot(tmp1['nw'], tmp1['time'].div(tseq), '-*r', label='ActiveFarm')
     b, = plt.plot(tmp2['nw'], tmp2['time'].div(tseq), 's-.g', label='PassiveFarm')
     c, = plt.plot(tmp3['nw'], tmp3['time'].div(tseq), '^--y', label='ParallelForOMP')
+    d, = plt.plot(tmp4['nw'], tmp4['time'].div(tseq), ':Dm', label='ParallelForFF')
+
     plt.yscale('log')
     plt.xscale('log')
     plt.ylabel('SpeedUp')
     plt.xlabel('NW')
     plt.title("SPEEDUP ::: Size: " + str(col))
-    plt.legend(handles=[a,b,c])
+    plt.legend(handles=[a,b,c,e])
     plt.show()
     fig.savefig('speedup' + str(col) + '.png')
 
@@ -61,22 +69,26 @@ for col in [256, 1024, 4096, 16384]:
     
     tt1 = par1[(par1.rowcol == col) & (par1.nw == 1)]['time'].iloc[0]
     tt2 = par2[(par2.rowcol == col) & (par2.nw == 1)]['time'].iloc[0]
-    tt3 = par1[(par3.rowcol == col) & (par3.nw == 1)]['time'].iloc[0]
+    tt3 = par3[(par3.rowcol == col) & (par3.nw == 1)]['time'].iloc[0]
+    tt4 = par4[(par4.rowcol == col) & (par4.nw == 1)]['time'].iloc[0]
 
     tmp1 = par1[(par1.rowcol == col)]
     tmp2 = par2[(par2.rowcol == col)]
     tmp3 = par3[(par3.rowcol == col)]
-    # plt.plot(seq['rowcol'], seq['time'], 'o-b')
-    plt.plot([0, 256], [0, 256])
+    tmp4 = par4[(par4.rowcol == col)]
+
+    plt.plot([0, 256], [0, 256],'k')
     a, = plt.plot(tmp1['nw'], tmp1['time'].div(tt1), '-*r', label='ActiveFarm')
     b, = plt.plot(tmp2['nw'], tmp2['time'].div(tt2), 's-.g', label='PassiveFarm')
     c, = plt.plot(tmp3['nw'], tmp3['time'].div(tt3), '^--y', label='ParallelForOMP')
+    d, = plt.plot(tmp4['nw'], tmp4['time'].div(tt4), ':Dm', label='ParallelForFF')
+
     plt.yscale('log')
     plt.xscale('log')
     # plt.ylabel('SpeedUp')
     plt.xlabel('NW')
     plt.title("SCALABILITY ::: Size: " + str(col))
-    plt.legend(handles=[a,b,c])
+    plt.legend(handles=[a,b,c,d])
     plt.show()
     fig.savefig('scalability' + str(col) + '.png')
 
