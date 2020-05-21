5b44e8c900fa33a4680a72826dce65ba71f89b44
ANDREA BRUNO <a.bruno25@C6320p-2.itc.unipi.it>
2020-05-21 16:28:05 +0200

    New Results

--- Assignment/FFCompressor/test_farm_split.sh
+++ Assignment/FFCompressor/test_farm_split.sh
@@ -1,6 +1,8 @@
 for nw in 1 4 8 16 32 64 128 256
     do 
     find . -name "*.tmp.part*" -delete
+    sleep 3
     find . -name "*.zip" -delete
-    ./ffc_farm_split $nw BIG/ 10000 >> res/farm_split.txt
+    sleep 2
+    ./ffc_farm_split $nw BIG/ 1000000 >> res/farm_split.txt
 done
