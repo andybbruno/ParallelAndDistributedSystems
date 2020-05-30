04ead5bc100ae2ed0c06af0e23b31e239db2ed85
ANDREA BRUNO <a.bruno25@C6320p-2.itc.unipi.it>
2020-05-30 12:40:31 +0200

    Results of seq version

--- Project/seq_main.cpp
+++ Project/seq_main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <cassert>
 #include "lib/tools.cpp"
 #include "lib/utimer.cpp"
 
@@ -34,40 +35,35 @@ void odd_even_sort(std::vector<int> &arr)
 
 int main(int argc, char *argv[])
 {
-    if (argc < 5)
+    if (argc < 4)
     {
-        std::cout << "\nUsage:   #Workers   #Elements   Range   Seed (0 means random)\n\n";
+        std::cout << "\nUsage:   #Elements   Range   Seed (0 means random)\n\n";
         exit(EXIT_FAILURE);
     }
 
-    int nw = atoi(argv[1]);
-    int dim = atoi(argv[2]);
-    int range = atoi(argv[3]);
+    int dim = atoi(argv[1]);
+    int range = atoi(argv[2]);
     if (range == 0)
         range = INT16_MAX;
-    int seed = atoi(argv[4]);
+    int seed = atoi(argv[3]);
     (seed == 0) ? srand(time(NULL)) : srand(seed);
 
-    if ((nw >= dim))
-    {
-        std::cout << "\n Too many workers! \n\n";
-        exit(EXIT_FAILURE);
-    }
     if (range < 0)
     {
         std::cout << "\n Range must be either positive or '0' for random! \n\n";
         exit(EXIT_FAILURE);
     }
 
-    std::vector<int> vec = tools::rand_vec(dim, limit);
+    std::vector<int> vec = tools::rand_vec(dim, range);
 
     auto begin = std::chrono::system_clock::now();
     odd_even_sort(vec);
     auto end = std::chrono::system_clock::now();
     auto elapsed = end - begin;
-    auto musec = std::chrono::duration_cast<std::chrono::milliseconds>(elapsed).count();
+    auto musec = std::chrono::duration_cast<std::chrono::microseconds>(elapsed).count();
     std::cout << musec << std::endl;
     // tools::print(vec);
+    assert(std::is_sorted(vec.begin(), vec.end()));
 
     return 0;
 }
---
+++ Project/start_seq.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo ""
+echo ""
+echo "Start: SEQUENTIAL"
+echo ""
+echo ""
+echo ""
+./seq_main.o $nw 100000 0 1 >> res/seq.txt
+echo ""
+echo ""
+echo ""
+echo "Finished!"
