4e0c425dd953ba88ce8a17ecba12e1b34537ae60
ANDREA BRUNO <a.bruno25@C6320p-2.itc.unipi.it>
2020-06-01 17:03:27 +0200

    Merged SH files

---
+++ Project/experiment.sh
@@ -0,0 +1,94 @@
+#!/bin/bash
+
+
+echo ""
+echo ""
+echo "Start: SEQUENTIAL"
+echo ""
+echo ""
+echo ""
+./seq.o 100000 0 1 >> res/seq.txt
+echo ""
+echo ""
+echo ""
+echo "Finished!"
+
+
+echo ""
+echo ""
+echo "Start: FARM"
+echo ""
+echo ""
+echo ""
+for nw in {1..50}
+    do
+    ./farm_tasks.o $nw 100000 0 1 >> res/farm_tasks.txt
+done
+echo ""
+echo ""
+echo ""
+echo "Finished!"
+
+
+echo ""
+echo ""
+echo "Start: FARM WITH VECTORS"
+echo ""
+echo ""
+echo ""
+for nw in {1..10}
+    do
+    ./farm_vectors.o $nw 100000 0 1 >> res/farm_vectors.txt
+done
+echo ""
+echo ""
+echo ""
+echo "Finished!"
+
+
+echo ""
+echo ""
+echo "Start: MASTER-WORKER"
+echo ""
+echo ""
+echo ""
+for nw in {1..50}
+    do
+    ./master_workers.o $nw 100000 0 1 >> res/master_workers.txt
+done
+echo ""
+echo ""
+echo ""
+echo "Finished!"
+
+
+echo ""
+echo ""
+echo "Start: FastFlow"
+echo ""
+echo ""
+echo ""
+for nw in {1..50}
+    do
+    ./fastflow.o $nw 100000 0 1  >> res/fastflow.txt
+done
+echo ""
+echo ""
+echo ""
+echo "Finished!"
+
+
+echo ""
+echo ""
+echo "Start: OMP"
+echo ""
+echo ""
+echo ""
+for nw in {1..50}
+    do
+    ./openmp.o $nw 100000 0 1 >> res/openmp.txt
+done
+echo ""
+echo ""
+echo ""
+echo "Finished!"
