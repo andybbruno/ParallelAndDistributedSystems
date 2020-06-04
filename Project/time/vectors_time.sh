9823d8e4ec24059a373ee4e7e99062917c971766
ANDREA BRUNO <a.bruno25@C6320p-2.itc.unipi.it>
2020-06-04 11:01:08 +0200

    Renamed folder "TIME" --> "time"

---
+++ Project/time/vectors_time.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+echo ""
+echo ""
+echo "Start: FARM with CHUNKS (EXECUTION TIME)"
+echo ""
+echo ""
+echo ""
+for nw in {1..10}
+    do
+    ./farm_vectors_exec.o $nw 100000 0 1 >> res/farm_vectors_exec.txt
+done
+echo ""
+echo ""
+echo ""
+echo "Finished!"
+
+
+echo ""
+echo ""
+echo "Start: FARM with CHUNKS (COMMUNICATION TIME)"
+echo ""
+echo ""
+echo ""
+for nw in {1..10}
+    do
+    ./farm_vectors_com.o $nw 100000 0 1 >> res/farm_vectors_com.txt
+done
+echo ""
+echo ""
+echo ""
+echo "Finished!"
